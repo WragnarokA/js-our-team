481473bb5f73ea9dd151a5f57610202f607a6984
WragnarokA <wilmer.arango.a@gmail.com>
2023-05-03 05:28:22 +0200

    array consol

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-console.log("ciao");
+// Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+// Angela Lopez	    Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg 
 
-console.log("asfvadfvsdfbsdbgsdbgsdbs");
+
+
+
+let team = [
+    {nome: "Wayne Barnett", professione: "Founder & CEO", img: "wayne-barnett-founder-ceo.jpg"},
+    {nome: "Angela Caroll", professione: "Chief Editor", img: "angela-caroll-chief-editor.jpg"},
+    {nome: "Walter Gordon", professione: "Officer Manager", img: "walter-gordon-office-manager.jpg"},
+    {nome: "Angela Lopez", professione: "Social Media Manager", img: "angela-lopez-social-media-manager.jpg"},
+    {nome: "Scot Estrada", professione: "Developer", img: "scott-estrada-developer.jpg"},
+    {nome: "Barbara Ramos", professione: "Graphic Designer", img: "barbara-ramos-graphic-designer.jpg"}
+]
+
+console.log(team);
+
